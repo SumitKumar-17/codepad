97eeea9c0be5f4d7e2e488716448f0bcf345b991
SumitKumar-17 <sumitkumar@kgpian.iitkgp.ac.in>
2024-12-04 11:13:44 +0530

    initial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+//! Server backend for the Rustpad collaborative text editor
+
+#![forbid(unsafe_code)]
+#![warn(missing_docs)]
+
+use warp::{filters::BoxedFilter, Filter, Reply};
+
+mod server;
+
+/// Construct routes for static files from React
+fn frontend() -> BoxedFilter<(impl Reply,)> {
+    warp::fs::dir("dist")
+        .or(warp::get().and(warp::fs::file("dist/index.html")))
+        .boxed()
+}
+
+/// Construct backend routes, including WebSocket handlers
+fn backend() -> BoxedFilter<(impl Reply,)> {
+    server::routes()
+}
+
+/// A combined filter handling all server routes
+fn server() -> BoxedFilter<(impl Reply,)> {
+    warp::path("api").and(backend()).or(frontend()).boxed()
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    pretty_env_logger::init();
+
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_| String::from("3030"))
+        .parse()
+        .expect("Unable to parse PORT");
+
+    warp::serve(server()).run(([0, 0, 0, 0], port)).await;
 }
