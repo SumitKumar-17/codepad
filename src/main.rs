92cc71e92747cc1959ff75399ba213ab66d4c47c
SumitKumar-17 <sumitkumar@kgpian.iitkgp.ac.in>
2024-12-04 11:32:30 +0530

    feat:added .env logger

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,22 @@
 #![warn(missing_docs)]
 
 use warp::{filters::BoxedFilter, Filter, Reply};
-
 mod server;
 
-/// Construct routes for static files from React
 fn frontend() -> BoxedFilter<(impl Reply,)> {
+    /// Construct routes for static files from React
     warp::fs::dir("dist")
         .or(warp::get().and(warp::fs::file("dist/index.html")))
         .boxed()
 }
 
-/// Construct backend routes, including WebSocket handlers
 fn backend() -> BoxedFilter<(impl Reply,)> {
+    /// Construct backend routes, including WebSocket handlers
     server::routes()
 }
 
-/// A combined filter handling all server routes
 fn server() -> BoxedFilter<(impl Reply,)> {
+    /// A combined filter handling all server routes
     warp::path("api").and(backend()).or(frontend()).boxed()
 }
 
